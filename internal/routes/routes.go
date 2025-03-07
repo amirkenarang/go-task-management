67e33b6c344ad4f8f35c1f32fafe83135cf95782
Amir <amir.kenarang@gmail.com>
2025-03-08 01:20:14 +0330

    Add CORS

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -3,9 +3,17 @@ package routes
 import (
 	"example.com/task-managment/internal/middlewares"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func RegisterRoutes(app *fiber.App) {
+	// Enable CORS
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*", // Allow all origins (change for production)
+		AllowMethods: "GET,POST,PUT,DELETE,OPTIONS",
+		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
+	}))
+
 	api := app.Group("/api") // Group all API routes under `/api`
 
 	UserRoutes(api)
